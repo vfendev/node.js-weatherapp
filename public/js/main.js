5a27c80d54ca725a2bafd34e26ad4fc77424f282
Velimir <velimir.patrnogic@gmail.com>
2020-08-01 20:11:23 +0200

    First change on main.js

--- public/js/main.js
+++ public/js/main.js
@@ -1,4 +1,4 @@
- const weatherForm = document.querySelector("form")
+const weatherForm = document.querySelector("form")
 const input = document.querySelector("input")
 const messageOne = document.querySelector("#message-1")
 const messageTwo = document.querySelector("#message-2")
